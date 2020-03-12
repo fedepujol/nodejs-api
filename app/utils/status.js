2149de6592cdad2bd21705f0f1c532c0acbcc1dd
fedepujol <federicodpujol@gmail.com>
2020-03-12 17:12:34 -0300

    Agrego dependecia de JWT

---
+++ app/utils/status.js
@@ -0,0 +1,24 @@
+var successMessage = {
+    status: 'success'
+}
+
+var errorMessage = {
+    status: 'error'
+}
+
+var status = {
+    success: 200,
+    created: 201,
+    noContent: 204,
+    badRequest: 400,
+    unAuthorized: 401,
+    notFound: 404,
+    conflict: 409,
+    errorServer: 500
+}
+
+export {
+    successMessage,
+    errorMessage,
+    status
+}
