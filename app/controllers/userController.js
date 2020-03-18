de240c99a51b18e1e06d8b8297352b509dff865b
fedepujol <federicodpujol@gmail.com>
2020-03-18 10:28:28 -0300

    Cambio userController a una clase

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -5,90 +5,88 @@ import {
     successMessage
 } from '../utils/status'
 
-async function getUsers(request, response) {
-    const q = 'SELECT * FROM users ORDER BY id ASC'
+class UserController {
+    constructor() {}
+    async getUsers(request, response) {
+        const q = 'SELECT * FROM users ORDER BY id ASC'
 
-    try {
-        var data = await dbQuery(q)
-        successMessage.data = data.rows
-        return response.status(status.success).json(successMessage)
-    } catch (error) {
-        errorMessage.message = error
-        return response.status(status.errorServer).send(errorMessage)
+        try {
+            var data = await dbQuery(q)
+            successMessage.data = data.rows
+            return response.status(status.success).json(successMessage)
+        } catch (error) {
+            errorMessage.message = error
+            return response.status(status.errorServer).send(errorMessage)
+        }
+    }
+
+    async createUser(request, response) {
+        const q = 'INSERT INTO users (name, email) VALUES ($1, $2)'
+
+        var name = request.body.name
+        var email = request.body.email
+
+        const value = [
+            name,
+            email
+        ]
+
+        try {
+            const data = await dbQuery(q, value)
+            successMessage.message = "User created"
+            return response.status(status.created).send(successMessage)
+        } catch (error) {
+            errorMessage.message = error
+            return response.status(status.errorServer).send(errorMessage)
+        }
+    }
+
+    async updateUser(request, response) {
+
+        if (request.method == 'PUT') {
+            console.log('PUT rece')
+        }
+
+        const q = 'UPDATE users SET name = $1, email = $2 WHERE id = $3'
+
+        const id = parseInt(request.params.id)
+
+        const {
+            name,
+            email
+        } = request.body
+
+        const params = [
+            name,
+            email,
+            id
+        ]
+
+        try {
+            const data = await dbQuery(q, params)
+            successMessage.message = "User updated"
+            return response.status(status.success).send(successMessage)
+        } catch (error) {
+            errorMessage.message = error
+            return response.status(status.errorServer).send(errorMessage)
+        }
+
+    }
+
+    async deleteUser(request, response) {
+        const q = 'DELETE FROM users WHERE id = $1'
+
+        const id = parseInt(request.params.id)
+
+        try {
+            const data = await dbQuery(q, id)
+            successMessage.message = "User deleted"
+            return response.status(status.success).send(successMessage)
+        } catch (error) {
+            errorMessage.message = error
+            return response.status(status.errorServer).send(errorMessage)
+        }
     }
 }
 
-async function createUser(request, response) {
-    const q = 'INSERT INTO users (name, email) VALUES ($1, $2)'
-
-    var name = request.body.name
-    var email = request.body.email
-
-    const value = [
-        name,
-        email
-    ]
-
-    try {
-        const data = await dbQuery(q, value)
-        successMessage.message = "User created"
-        return response.status(status.created).send(successMessage)
-    } catch (error) {
-        errorMessage.message = error
-        return response.status(status.errorServer).send(errorMessage)
-    }
-}
-
-async function updateUser(request, response) {
-
-    if (request.method == 'PUT') {
-        console.log('PUT rece')
-    }
-
-    const q = 'UPDATE users SET name = $1, email = $2 WHERE id = $3'
-
-    const id = parseInt(request.params.id)
-
-    const {
-        name,
-        email
-    } = request.body
-
-    const params = [
-        name,
-        email,
-        id
-    ]
-
-    try {
-        const data = await dbQuery(q, params)
-        successMessage.message = "User updated"
-        return response.status(status.success).send(successMessage)
-    } catch (error) {
-        errorMessage.message = error
-        return response.status(status.errorServer).send(errorMessage)
-    }
-
-}
-
-async function deleteUser(request, response) {
-    const q = 'DELETE FROM users WHERE id = $1'
-
-    const id = parseInt(request.params.id)
-
-    try {
-        const data = await dbQuery(q, id)
-        successMessage.message = "User deleted"
-        return response.status(status.success).send(successMessage)
-    } catch (error) {
-        errorMessage.message = error
-        return response.status(status.errorServer).send(errorMessage)
-    }
-}
-
-export {
-    getUsers,
-    createUser,
-    updateUser,
-    deleteUser
-}
+export default new UserController()
--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,17 +1,12 @@
-import {
-    createUser,
-    updateUser,
-    deleteUser,
-    getUsers
-} from "../controllers/userController"
 
 import express from "express"
+import userController from "../controllers/userController"
 
 const router = express.Router()
 
-router.get('/users', getUsers)
-router.post('/users', createUser)
-router.put('/users/:id', updateUser)
-router.delete('/users/:id', deleteUser)
+router.get('/users', userController.getUsers)
+router.post('/users', userController.createUser)
+router.put('/users/:id', userController.updateUser)
+router.delete('/users/:id', userController.deleteUser)
 
 export default router
